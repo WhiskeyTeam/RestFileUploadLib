f4449d96fc052c7b2ffdc80a28ca4758fcd39de0
Awesome <awesome_devnet@outlook.com>
2024-08-06 16:17:25 +0900

    외부접속 가능하도록 모듈 오버로딩

--- src/main/java/com/whiskey/libs/file/FileUploader.java
+++ src/main/java/com/whiskey/libs/file/FileUploader.java
@@ -13,11 +13,28 @@ import java.util.UUID;
 @Getter
 public class FileUploader {
     private final String filePath;
+    private final String ENDPOINT = "/file/upload";
 
-    private final String REQUEST_URL = "http://localhost:8083/file/upload";
+    private String DEFAULT_HOST = "http://localhost:8083";
+    private String userHost;
 
     public FileUploader(String filePath) {
         this.filePath = filePath;
+
+        // 파일명이 없을 경우 예외 발생
+        if (filePath == null || filePath.isEmpty()) {
+            throw new IllegalArgumentException("잘못된 파일 경로입니다.");
+        }
+    }
+
+    public FileUploader(String filePath, String host) {
+        this.userHost = host;
+        this.filePath = filePath;
+
+        // host앞에 http://가 없을 경우 예외 발생
+        if (!host.startsWith("http://")) {
+            throw new IllegalArgumentException("http://를 포함한 host를 입력해주세요.");
+        }
 
         // 파일명이 없을 경우 예외 발생
         if (filePath == null || filePath.isEmpty()) {
@@ -39,6 +56,7 @@ public class FileUploader {
         nameGroup.setOriginalFileName(targetFile.getName());
         nameGroup.setUuidFileName(newFileName);
 
+        // Request Body 생성
         MultipartEntityBuilder builder = MultipartEntityBuilder.create();
         builder.addBinaryBody(
                 "file",
@@ -49,12 +67,23 @@ public class FileUploader {
 
         HttpEntity multipart = builder.build();
 
-        HttpPost httpPost = new HttpPost(REQUEST_URL);
+        // user_host가 null일 경우 기본 host로 설정
+        HttpPost httpPost;
+        if (userHost == null) {
+            httpPost = new HttpPost(DEFAULT_HOST + ENDPOINT);
+        } else {
+            httpPost = new HttpPost(userHost + ENDPOINT);
+        }
         httpPost.setEntity(multipart);
 
         // 파일 업로드
         CloseableHttpClient httpClient = HttpClients.createDefault();
         var response = httpClient.execute(httpPost);
+
+        // response code가 200이 아닐 경우 예외 발생
+        if (response.getStatusLine().getStatusCode() != 200) {
+            throw new Exception("파일 업로드에 실패했습니다.");
+        }
 
         return nameGroup;
     }
